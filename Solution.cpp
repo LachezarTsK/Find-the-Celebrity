2a953926a2e7ce3420b4605091a27e2cc4666552
Lachezar Kutsarov <lkutsarov@gmail.com>
2022-01-03 22:10:14 +0100

    Update Solution.cpp

--- Solution.cpp
+++ Solution.cpp
@@ -7,12 +7,13 @@ public:
 
     int findCelebrity(int n) {
         totalPeople = n;
+        int potentialCelebrity = 0;
         for (int person = 0; person < totalPeople; person++) {
-            if (isCelebrity(person)) {
-                return person;
+            if (knows(potentialCelebrity, person)) {
+                potentialCelebrity = person;
             }
         }
-        return -1;
+        return isCelebrity(potentialCelebrity) ? potentialCelebrity : -1;
     }
 
     bool isCelebrity(int& person) {
@@ -32,9 +33,9 @@ public:
      * The functions 'knows', displayed here, is for information purposes only.
      * This function is implemented internally by leetcode.com and should not be
      * included in the code, when running the solution on the website.
-	
+     
     bool knows(int a, int b) {
-            returns 'true', if 'a' knows 'b', otherwise returns 'false'.
+       returns 'true', if 'a' knows 'b', otherwise returns 'false'.
     }
-     */
+    */
 };
