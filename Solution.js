d01be756adbe08ffd21d3a85f668eaf777f6b316
Lachezar Kutsarov <lkutsarov@gmail.com>
2022-01-03 20:55:48 +0100

    Create Solution.js

---
+++ Solution.js
@@ -0,0 +1,46 @@
+
+/**
+ * 'boolean knows(int a, int b)' API:
+ * 
+ * The functions 'knows', displayed here, is for information purposes only.
+ * This function is implemented internally by leetcode.com and should not be
+ * included in the code, when running the solution on the website.
+ * 
+ * @param {integer} person a
+ * @param {integer} person b
+ * @return {boolean} whether a knows b
+ * knows = function(a, b) {
+ *  returns 'true', if 'a' knows 'b', otherwise returns 'false'.
+ * };
+ */
+
+/**
+ * @param {function} knows()
+ * @return {function}
+ */
+var solution = function (knows) {
+    /**
+     * @param {integer} n Total people
+     * @return {integer} The celebrity
+     */
+    return function (n) {
+        const totalPeople = n;
+        for (let person = 0; person < totalPeople; person++) {
+            if (isCelebrity(person, totalPeople)) {
+                return person;
+            }
+        }
+        return -1;
+    };
+
+    function isCelebrity(person, totalPeople) {
+        for (let anotherPerson = 0; anotherPerson < totalPeople; anotherPerson++) {
+            if (anotherPerson !== person) {
+                if (knows(person, anotherPerson) || !knows(anotherPerson, person)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
